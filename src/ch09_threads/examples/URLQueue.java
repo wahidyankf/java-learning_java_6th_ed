4d02a6c5fd113f29f77b6ad6da62062b9cfa46fd
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-10-13 06:56:04 +0700

    go until urlqueue

--- src/ch09_threads/examples/URLQueue.java
+++ src/ch09_threads/examples/URLQueue.java
@@ -1,5 +1,23 @@
 package ch09_threads.examples;
 
+import java.util.LinkedList;
+
 public class URLQueue {
+  LinkedList<String> urlQueue = new LinkedList<String>();
+
+  public synchronized void addURL(String url) {
+    urlQueue.add(url);
+  }
+
+  public synchronized String getURL() {
+    if (!urlQueue.isEmpty()) {
+      return urlQueue.removeFirst();
+    }
+    return null;
+  }
+
+  public boolean isEmpty() {
+    return urlQueue.isEmpty();
+  }
 
 }
